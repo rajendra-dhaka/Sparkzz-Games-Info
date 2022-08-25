0ee41d7b0efbc50761f991d6bf677072624ae490
rajendra-dhaka <rajendradhaka13@gmail.com>
2022-08-25 07:03:29 +0530

    2nd commit

---
+++ src/api.js
@@ -0,0 +1,36 @@
+//Base URL
+const base_url = `https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}`;
+
+//Getting the date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1; //Adding 1 bcz this gives index of month
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+//Current Day/Month/Year
+
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+// GAMES
+const popular_games = `&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+
+export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+//Redux Setup
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers";
+//Hookup Redux to React
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
